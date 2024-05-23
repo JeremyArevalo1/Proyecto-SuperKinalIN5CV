78981b8e56a93d1c8552cc6c06e4da1334d5321e
JeremyArevalo1 <jarevalo-2023528@kinal.edu.gt>
2024-05-22 23:10:23 -0600

    actualizacion del proyecto

--- src/org/jeremyarevalo/controller/MenuLoginController.java
+++ src/org/jeremyarevalo/controller/MenuLoginController.java
@@ -6,6 +6,10 @@
 package org.jeremyarevalo.controller;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,7 +17,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.jeremyarevalo.dao.Conexion;
+import org.jeremyarevalo.model.Usuario;
 import org.jeremyarevalo.system.Main;
+import org.jeremyarevalo.utils.PasswordUtils;
+import org.jeremyarevalo.utils.SuperKinalAlert;
 
 /**
  * FXML Controller class
@@ -22,6 +30,12 @@ import org.jeremyarevalo.system.Main;
  */
 public class MenuLoginController implements Initializable {
     private Main stage;
+    private int op = 0;
+    
+    private static Connection conexion = null;
+    private static PreparedStatement statement = null;
+    private static ResultSet resultSet = null;
+    
     
     @FXML
     TextField tfUsuario;
@@ -33,7 +47,29 @@ public class MenuLoginController implements Initializable {
     @FXML
     public void handleButtonAction(ActionEvent event){
         if(event.getSource() == btnIniciar){
-            
+            Usuario usuario = buscarUsuario();
+            if(op == 0){
+                if(usuario != null){  
+                    if(PasswordUtils.getInstance().checkPassword(pfContrasenia.getText(), usuario.getContrasenia())){
+                        SuperKinalAlert.getInstance().alertaSaludo(usuario.getUsuario());
+                        if(usuario.getNivelAccesoId() == 1){
+                            btnRegistrarU.setDisable(false);
+                            btnIniciar.setText("ir al menu");
+                            op = 1;
+                        }else if(usuario.getNivelAccesoId() == 2){
+                            stage.menuPrincipalView();
+                        }
+                    }else{
+                        SuperKinalAlert.getInstance().mostrarAlertaInfo(815);
+                    }  
+                }else{
+                    SuperKinalAlert.getInstance().mostrarAlertaInfo(805);
+                }
+                }else{
+                    stage.menuPrincipalView();
+                }
+        }else if(event.getSource() == btnRegistrarU){
+            stage.menuRegistroView();
         }
     }
     
@@ -49,6 +85,43 @@ public class MenuLoginController implements Initializable {
         this.stage = stage;
     }
     
-    
+    public Usuario buscarUsuario(){
+        Usuario usuario = null;
+        try{
+            conexion = Conexion.getInstance().obtenerConexion();
+            String sql = "call sp_buscarUsuarios(?)";
+            statement = conexion.prepareStatement(sql);
+            statement.setString(1, tfUsuario.getText());
+            resultSet = statement.executeQuery();
+            
+            if(resultSet.next()){
+                int usuarioId = resultSet.getInt("usuarioId");
+                String user = resultSet.getString("usuario");
+                String contrasenia = resultSet.getString("contrasenia");
+                int nivelAccesoId = resultSet.getInt("nivelAccesoId");
+                int empleadoId = resultSet.getInt("empleadoId");
+                
+                usuario = new Usuario(usuarioId, user, contrasenia, nivelAccesoId, empleadoId);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }finally{
+            try{
+                if(statement != null){
+                    statement.close();
+                }
+                if(resultSet != null){
+                    resultSet.close();
+                }
+                if(conexion != null){
+                    conexion.close();
+                }
+                
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return usuario;
+    }
     
 }
